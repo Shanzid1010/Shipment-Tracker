26bbb8aa16a464574037fa5ce7adae6754f93c2f
Shanzid1010 <hossainmdshanzid@gmail.com>
2025-11-17 23:51:55 +0600

    Update settings.py
    new

--- ShipmentProject/settings.py
+++ ShipmentProject/settings.py
@@ -67,9 +67,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 20, # ⬅️ এই লাইনটি যোগ করুন
-        }
     }
 }
 
